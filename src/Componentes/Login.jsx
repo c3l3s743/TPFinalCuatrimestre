9ef855655b7de534d4c8769f361e2f71add6458e
Joako <joako181105@gmail.com>
2024-06-24 02:37:54 -0300

    se implemento correctamente la funcion de iniciar sesion

--- src/Componentes/Login.jsx
+++ src/Componentes/Login.jsx
@@ -3,17 +3,48 @@ import { Component } from "react";
 export default class Login extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            nombre:'',
+            contraseña:''
+           
+        }
     }
 
+    CambiarNombre = (e) => {
+        this.setState({ nombre: e.target.value });
+    }
+
+    CambiarContraseña = (e) => {
+        this.setState({ contraseña: e.target.value });
+    }
+    
+    Enviar = (e) =>{
+    
+        const { nombre, contraseña } = this.state;
+
+         console.log("Nombre de usuario:", this.state.nombre);
+         console.log("Contraseña:", this.state.contraseña);
+        //Mensaje para meli, esto despues lo quito, es solo para comprobar que la informacion se esta enviando de manera correcta
+    }
     render() {
+
         return (
             <section>
                 <h1>Login</h1>
-                <form>
-                    <input type="text" name="" id="" />
-                    <input type="password" name="" id="" />
-                    <button type="submit">Iniciar Sesion</button>
+                <form className="formulario" onSubmit={this.Enviar}>
+                    <input 
+                    type="text" 
+                    value={this.state.nombre} 
+                    onChange={this.CambiarNombre}
+                    placeholder="Nombre de usuario"
+                   />
+                    <input 
+                    type="password" 
+                    value={this.state.contraseña}
+                    onChange={this.CambiarContraseña}
+                    placeholder="Contraseña"
+                     />
+                    <button type="button" onClick={this.Enviar}>Iniciar Sesion</button>
                     
                 </form>
             </section>
